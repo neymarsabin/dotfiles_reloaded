29d2e30d82f503286add2a0c21fc7c7bb319b19f
neymarsabin <reddevil.sabin@gmail.com>
2017-01-25 19:10:27 +0545

    add i3,mplayer and new theme for rofi

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# always load i3 from a config file and stuffs
+### copy the i3 configuration file to the home folder
+cp $config_dir/i3config ~/.config/i3/config 
 
+# always load i3 from a config file and stuffs
 alias i3='i3 -c $config_dir/i3config'
 
 
---
+++ scripts/mplayer.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# mplayer configurations
+alias mplayer='mplayer -include $config_dir/mplayer.conf'
