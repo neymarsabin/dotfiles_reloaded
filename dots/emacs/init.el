c592b9a095d4124f6b017acd8a25561281a5cdbd
neymarsabin <reddevil.sabin@gmail.com>
2017-04-18 21:25:17 +0545

    change design of status bar

--- dots/emacs/init.el
+++ dots/emacs/init.el
@@ -187,29 +187,28 @@
 (define-key global-map "\C-ca" 'org-agenda)
 
 ;; ;;org mode for blogging and stuffs
-;; (setq org-publish-project-alist
-;;       '(
-;; 				("org-neymarsabin"
-;;           ;; Path to your org files.
-;; 				 :base-directory "/mnt/hackit/codeds/orgblog/posts"
-;; 				 ;; :base-directory "/mnt/hackit/codeds/github-website/org-web-site/org/"
-;;           :base-extension "org"
-;;           ;; Path to your Jekyll project.
-;;           :publishing-directory "/mnt/hackit/codeds/jekyll/space/_posts"
-;;           :recursive t
-;; 					:publishing-function org-html-publish-to-html
-;;           :headline-levels 4 
-;;           :html-extension "html"
-;; 					:body-only t
-;; 					)
-;; 				("org-static-sabin"
-;;           :base-directory "/mnt/hackit/codeds/orgblog/posts"
-;;           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
-;;           :publishing-directory "/mnt/hackit/codeds/orgblog"
-;;           :recursive t
-;;           :publishing-function org-publish-attachment)
-;; 				("helloworld" :components ("org-neymarsabin" "org-static-sabin"))
-;;  				))
+(setq org-publish-project-alist
+      '(
+				("org-neymarsabin"
+          ;; Path to your org files.
+				 :base-directory "/mnt/hackit/codeds/orgblog/posts"
+				 ;; :base-directory "/mnt/hackit/codeds/github-website/org-web-site/org/"
+          :base-extension "org"
+          ;; Path to your Jekyll project.
+          :publishing-directory "/mnt/hackit/codeds/orgblog/_posts"
+          :recursive t
+					:publishing-function org-html-publish-to-html
+          :headline-levels 4 
+          :html-extension "html"
+					:body-only t
+					)
+				;; ("org-static-sabin"
+        ;;   :base-directory "/mnt/hackit/codeds/cloned/blogcopy/posts"
+        ;;   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+        ;;   :publishing-directory "/mnt/hackit/codeds/cloned/console"
+        ;;   :recursive t
+        ;;   :publishing-function org-publish-attachment)
+ 				))
 
 ;;load theme 
 (load-theme 'wombat t)
@@ -242,7 +241,23 @@
 				 :publishing-directory "/mnt/hackit/codeds/orgblog"
 				 :publishing-function(org-html-publish-to-html)
 				 :html-preamble nil
-				 :html-postamble nil )))
+				 :html-postamble nil )
+			("bootstrap-static"
+			 :base-directory "/home/neymar/public_html/orgblog/"
+			 :html-extension "html"
+			 :base-extension "org"
+			 :publishing-directory "/home/neymar/public_html/"
+			 :publishing-function org-twbs-publish-to-html
+			 :with-sub-superscript nil
+			 )
+			("bootstrap"
+			 :base-directory "/home/neymar/public_html/orgblog/posts"
+			 :html-extension "html"
+			 :base-extension "org"
+			 :publishing-directory "/home/neymar/public_html/html"
+			 :publishing-function org-twbs-publish-to-html
+			 :with-sub-superscript nil
+			 )))
 
 ;;reload emacs without closing 
 (defun reload-user-init-file()
@@ -427,4 +442,5 @@
 ;; )
 
 ;; (setq org-export-filter-final-output-functions  '(my-final-filter) )
+
 
