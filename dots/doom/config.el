85241f32ed5f5e87c8415dbb8f0c10e8334ddc76
neymarsabin <reddevil.sabin@gmail.com>
2024-01-17 02:16:00 +0545

    emacs config for hugo folder

--- dots/doom/config.el
+++ dots/doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Monaco" :size 12 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Monaco" :size 13))
+(setq doom-font (font-spec :family "Monaco" :size 15 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Monaco" :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'wombat)
+(setq doom-theme 'atom-one-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -42,6 +42,7 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/projects/mine/myself/the-new-org/doom/capture")
 (setq org-journal-dir "~/projects/mine/myself/the-new-org/doom/capture")
+(setq org-hugo-base-dir "~/projects/mine/portfolio")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -178,12 +179,12 @@ I wish I could say I wrote this function, copies everything of the function inse
 Rely on your LSP for indentation, couldn't write a single thing on indenting."
   (interactive "P")
   (insert-pair arg ?\{ ?\}))
-;; (global-set-key (kbd "M-S-{") 'insert-curves)
+(global-set-key (kbd "M-S-{") 'insert-curves)
 
-(defun insert-bigboots (&optional arg)
-  "Inserts [ ] curves to the selected region.
-Same here :D I wish I could say I wrote this function, copies everything of the function insert-parenthesis.
-Rely on your LSP for indentation, couldn't write a single thing on indenting."
-  (interactive "P")
-  (insert-pair arg ?\[ ?\]))
-;; (global-set-key (kbd "M-S-^") 'insert-bigboots)
+;; (defun insert-bigboots (&optional arg)
+;;   "Inserts [ ] curves to the selected region.
+;; Same here :D I wish I could say I wrote this function, copies everything of the function insert-parenthesis.
+;; Rely on your LSP for indentation, couldn't write a single thing on indenting."
+;;   (interactive "P")
+;;   (insert-pair arg ?\[ ?\]))
+;; ;; (global-set-key (kbd "M-S-^") 'insert-bigboots)
--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
 #for my shell
-
-###### some aliases #################
-
-# alias for Discord app
-alias discord='/mnt/hackit/codeds/Discord/Discord'
-#basic navigation of my folders to aliases
-alias codeds='cd /mnt/hackit/codeds '
-alias suta='sudo systemctl start suspend.target'
-alias ngrok='/mnt/hackit/everything/ngrok/ngrok'
-alias hackit='/mnt/hackit/'
-
-#back made easy
-# alias ..='cd ..'
-# alias ...= 'cd ../../'
+alias ..='cd ..'
+alias ...= 'cd ../../'
 
 # screenfetch
 # [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -25,53 +13,75 @@ alias cp='cp -i -v '
 alias rm='rm -i'
 #mv command with interactive
 alias mv='mv -i'
-# bundle to b
-alias b='bundle'
-alias be='bundle exec'
-alias ls='exa'
 
 # some git aliases
 alias glol='git log --oneline --all --graph --decorate'
-# power options laptop
-alias gotosleep='sudo systemctl suspend'
-alias gotodie='shutdown -h'
-alias refresh='reboot'
 
+# I am on macOS these days, group these with comments
+# for gpu settings
+# xrandr --setprovideroffloadsink 1 0
+# DRI_PRIME=1 glxinfo 1> /dev/null
 
-### some environment variables #####
-
+# start ibus for language selection
+# ibus-daemon -drx
+#
+#### some environment variables #####
 # environment variables
 # export PATH=$PATH:/home/neymar/Android/Sdk/tools
 # export ANDROID_HOME=/home/neymar/Android/Sdk
 
-# for laravel
-# export PATH=$PATH:/home/neymar/.config/composer/vendor/bin
+# User configuration
 
-#for emacs dependency manager cask
-export PATH="/home/neymar/.cask/bin:$PATH"
-export EDITOR="emacsclient -nw"
-# exporting path for jruby exectuionals
-export PATH="/mnt/hackit/codeds/jruby/jruby-9.1.8.0/bin:$PATH"
-export CLASSPATH="/home/neymar/Downloads/mysql-connector-java-5.1.42/mysql-connector-java-5.1.42-bin.jar:$CLASSPATH"
-#for rvm and stuffs
+# export MANPATH="/usr/local/man:$MANPATH"
 
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
 
-# my configs directory
-export config_dir="/mnt/hackit/codeds/dotfiles/dots"
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
 
-############ some useful functions #############
-function mount_usb {
-		sudo mount -o gid=users,fmask=113,dmask=002 $1 /mnt/pendrive_is_on_high
-}
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
 
-function umount_usb {
-    sudo umount /mnt/pendrive_is_on_high
-}
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
 
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-# for gpu settings
-xrandr --setprovideroffloadsink 1 0
-DRI_PRIME=1 glxinfo 1> /dev/null
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# start ibus for language selection
-ibus-daemon -drx
+## fixing go binary path
+export PATH=$PATH:/usr/local/go/bin
+
+# alias ls into exa
+alias ls='exa -lhSHgi --git'
+
+echo 'ulimit -n 12288' >> ~/.bash_profile
+export PATH=$PATH:/Users/neymarsabin/Documents/mongo/bin
+export PATH="/opt/homebrew/opt/mongodb-community@4.4/bin:$PATH"
+
+# chatgpt command line interface
+export PATH=$PATH:/Users/neymarsabin/.config/emacs/bin
+
+# add golang path to emacs
+export GOPATH="$(go env GOPATH)"
+export PATH="${PATH}:${GOPATH}/bin"
+
+# load doom config from dotfiles repo, !home folder
+dotfile_location=projects/pet/dotfiles_reloaded
+export DOOMDIR=$HOME/$dotfile_location/dots/doom
