dcf262ad94a22869674344aa0fa88242abf44f40
neymarsabin <reddevil.sabin@gmail.com>
2024-03-21 13:51:48 +0545

    update and source aliases

---
+++ scripts/aliases.sh
@@ -0,0 +1,41 @@
+#!/bin/env sh
+# to use these aliases in your terminal, you need to source this file
+# source aliases.sh
+
+# aliases for vim
+alias vi='nvim'
+alias vim='nvim'
+alias nvim='nvim'
+alias nvmi='nvim'
+alias nivm='nvim'
+
+# aliases for ping
+alias p8='ping 8.8.8.8'
+
+# aliases for git
+alias gst='git status'
+alias gco='git checkout'
+alias gci='git commit'
+alias gcm='git commit -m'
+alias gaa='git add .'
+alias gpl='git pull'
+alias gps='git push'
+alias gbr='git branch'
+alias gdf='git diff'
+alias glog='git log'
+alias gcl='git clone'
+alias glol='git log --oneline --pretty --graph'
+
+# aliases for tools
+alias ll='ls -l'
+alias la='ls -la'
+alias rm='rm -i'
+alias cp='cp -i -v'
+alias mv='mv -i -v'
+alias mkdir='mkdir -p'
+alias grep='grep --color=auto'
+alias df='df -h'
+alias du='du -h'
+alias free='free -h'
+alias top='htop'
+alias psg='ps aux | grep '
