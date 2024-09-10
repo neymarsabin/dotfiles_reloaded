f0bf578a6cf18b071e099fdc269368b3aecc2ddb
neymarsabin <reddevil.sabin@gmail.com>
2024-09-10 15:52:16 +0545

    i3config updates

--- dots/nvim/lua/neymarsabin/plugins/lsp.lua
+++ dots/nvim/lua/neymarsabin/plugins/lsp.lua
@@ -28,6 +28,7 @@ return {
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("<leader>ge", vim.diagnostic.open_float, "Popup Errors")
 
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if client and client.server_capabilities.documentHighlightProvider then
