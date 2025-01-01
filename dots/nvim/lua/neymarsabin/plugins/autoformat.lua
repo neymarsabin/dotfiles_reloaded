b7f5e959353fc33bf10876b811f6300763cdd475
neymarsabin <reddevil.sabin@gmail.com>
2025-01-01 22:54:06 +0545

    some prettier configuration

--- dots/nvim/lua/neymarsabin/plugins/autoformat.lua
+++ dots/nvim/lua/neymarsabin/plugins/autoformat.lua
@@ -3,8 +3,8 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = {
-			timeout_ms = 200,
-			lsp_fallback = true,
+			timeout_ms = 500,
+			lsp_fallback = "fallback",
 		},
 		formatters_by_ft = {
 			lua = { "stylua" },
