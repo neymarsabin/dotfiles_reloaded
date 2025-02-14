55b8fbe48038c42ed03b8c9d197d047e020f2bf9
neymarsabin <reddevil.sabin@gmail.com>
2025-02-14 17:32:50 +0545

    improve fzfdir

--- scripts/fzfdir.sh
+++ scripts/fzfdir.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env sh
-# I use sh, because I have aliases all over, which I don't want the script to take into consideration
-# Use at your own risk: I have seen bugs that create infinite tmux sessions :D
 # define the fzf command: ./.fzfdir.sh pet
 # syntax: ./.fzfdir.sh <working_directory>
-# note: I have base_dir as ~/projects because I have all of my projects in ~/projects
+# NOTE: this script does not work with directories with special characters
+#
 # FZF_COMMAND="fzf-tmux"
 FZF_COMMAND="fzf-tmux -p --with-nth 1"
 
 # find in directories
 workdir=$1
 base_dir=~/bucks
-find_dir=$base_dir/*/*
+find_dir="$base_dir/*/*"
 
 # Execute Command
-RESULT=$(ls -d $find_dir | $FZF_COMMAND)
+searched_dir=$(ls -d $find_dir | $FZF_COMMAND)
+RESULT=$(echo "$searched_dir" | sed 's#.*/##')
 
 # Do nothing if result is EMPTY
 # Works in case if you do not find something and press ESC
 if [[ -n "$RESULT" ]]; then
     # create a new tmux session and attach to it
-    window_name=$RESULT
-    session_name="$window_name"
-    workdir=$RESULT
-    send_command="cd $workdir"
+    window_name="server"
+    session_name="$RESULT"
+    workdir="$base_dir/$1/$RESULT"
 
-    if ! tmux has-session -t $session_name 2>/dev/null; then
-    ## create new session, provide SESSION and WINDOW name
-    new_session=$(TMUX= tmux new-session -A -d -s $session_name -n $window_name)
-
-    ## switch and cd into project
-    tmux switch-client -t $session_name
-    tmux send-keys -t $session_name:$window_name "$send_command; clear" C-m
-    else
-      tmux switch-client -t $session_name
+    if ! tmux has-session -t "$session_name" 2>/dev/null; then
+      tmux new-session -d -s "$session_name" -n "$window_name" -c "$searched_dir"
     fi
+
+    tmux switch-client -t "$session_name"
 fi
