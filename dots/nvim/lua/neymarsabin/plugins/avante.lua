1d40ccf0c548df3a846f13c309df2d1d3269ccd8
neymarsabin <reddevil.sabin@gmail.com>
2025-05-19 18:10:15 +0545

    avante setup

--- dots/nvim/lua/neymarsabin/plugins/avante.lua
+++ dots/nvim/lua/neymarsabin/plugins/avante.lua
@@ -4,7 +4,7 @@ return {
 	lazy = false,
 	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
 	opts = {
-		provider = "openai",
+		provider = "copilot",
 	},
 	build = "make",
 	dependencies = {
